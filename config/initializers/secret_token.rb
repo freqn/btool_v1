e42bd73063b90dd057fa174d3996727db28d6e59
freqn <freqnlabs@gmail.com>
2014-03-10 12:21:59 -0400

    Add controller action for listing posts

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Btool::Application.config.secret_key_base = '91aef1412d9f2c24bb96162fc0c181ed10f10e9a2563c428455472fb45629f5715d298cb10dd44af9776ee872e1a3a3e3860d538bcc75836cd8f7ff589e7f907'
